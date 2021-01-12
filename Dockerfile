ca6665f0a3c52a06123fac8d9d630210c390ad3b
Jules <bigaston1@gmail.com>
2021-01-12 11:30:41 +0100

    Dernière version, qui marche

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM debian
 COPY ./startup.sh /startup.sh
 COPY ./icecast.xml /etc/icecast2/icecast.xml
 
-RUN apt update
-RUN yes no | apt install -y icecast2
+RUN apt-get update
+RUN yes no | apt-get install -y --no-install-recommends icecast2
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
 
 CMD /bin/bash /startup.sh
--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sed -i "s/MAX_CLIENT/${MAX_CLIENT:-100}/g" /etc/icecast2/icecast.xml
 
 sed -i "s/MAX_SOURCE/${MAX_SOURCE:-2}/g" /etc/icecast2/icecast.xml
@@ -10,5 +12,4 @@ sed -i "s/ADMIN_USERNAME/${ADMIN_USERNAME:-admin}/g" /etc/icecast2/icecast.xml
 
 sed -i "s/ADMIN_PASSWORD/${ADMIN_PASSWORD:-hackme}/g" /etc/icecast2/icecast.xml
 
-su icecast2 -s /bin/bash
-/usr/bin/icecast2 -c /etc/icecast2/icecast.xml
+su - icecast2 -s /bin/bash -c "/usr/bin/icecast2 -c /etc/icecast2/icecast.xml"
